b216d26a027a1390b404e3476c500a7a4bf91487
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 11:18:41 +0700

    Update project.py

--- project.py
+++ project.py
@@ -12,13 +12,13 @@ with col1:
   st.image("Website.png", width=900)
 
 with col2:
-  st.write("")
+  st.header("WE HELP COMPANIES GROW")
+  st.write("Become data-driven today and scale your business.We take the time to understand your business and how data can help you to achieve your goals.")
 
 with col3:
   st.write("")
   
-st.title("WE HELP COMPANIES GROW")
-st.header("Become data-driven today and scale your business.We take the time to understand your business and how data can help you to achieve your goals.")  
+  
 
 option = st.sidebar.selectbox("HAB LABS Services", ('Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
 
