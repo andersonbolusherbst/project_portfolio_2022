75dd7a0fd78f1298c54f6fefeeb17a5df51fdf13
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 11:14:54 +0700

    Update project.py

--- project.py
+++ project.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 import requests
 
-from PIL import Image
-background = Image.open('Website.png')
+#from PIL import Image
+#background = Image.open('Website.png')
 
 col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
-  st.image(background, width=1000)
+  st.image("Website.png", width=1000)
 
 with col2:
   st.write("")
