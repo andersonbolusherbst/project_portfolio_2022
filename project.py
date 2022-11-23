3ca8019b70b92b76208deeba319ce0d3364c38b9
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 13:55:11 +0700

    Update project.py

--- project.py
+++ project.py
@@ -53,7 +53,7 @@ if option == "Machine Learning":
     
     st.markdown(""" div.stButton > button:first-child {
     background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
-    },""" unsafe_allow_html=True)
+    }""", unsafe_allow_html=True)
     
     
 
