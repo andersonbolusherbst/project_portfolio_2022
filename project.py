b46b25a4d2827825c0dbea584cae10a9c1f37844
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 10:47:05 +0700

    Update project.py

--- project.py
+++ project.py
@@ -7,14 +7,11 @@ st.image("Website.png")
 
 option = st.sidebar.selectbox("HAB LABS Services", ('Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
 
-if option == "Analytics":
-  
-if option == "Data Infrastructure":
-  
 if option == "Machine Learning":
   
   
-if option == "Custom Web Apps":
+
+
  
 
 
