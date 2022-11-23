f8c7758c5c9e45323397eca61ea7213257c34da4
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 11:07:12 +0700

    Update project.py

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ background = Image.open('Website.png')
 col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
-  st.write(background, width=1000)
+  st.image(background, width=1000)
 
 with col2:
   st.write("")
