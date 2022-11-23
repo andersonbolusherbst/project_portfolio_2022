8ccc5d376bad437908211a28c0bacc8f9f70fdc5
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 10:52:58 +0700

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import requests
 
-st.image("Website.png")
+st.image("Website.png", width=1000, height=1500)
 
 option = st.sidebar.selectbox("HAB LABS Services", ('Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
 
