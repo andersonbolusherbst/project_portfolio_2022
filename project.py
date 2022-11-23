3006cc11da28ed8cc5c7691313455ec0f9d9abed
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 13:10:25 +0700

    Update project.py

--- project.py
+++ project.py
@@ -6,27 +6,32 @@ import requests
 #from PIL import Image
 #background = Image.open('Website.png')
 
-col1, col2, col3 = st.columns([1,1,1])
+option = st.sidebar.selectbox("HAB LABS Services", ( "Home", 'Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
 
-with col1:
-  st.image("Website.png", width=900)
+if option == "Home":
 
-with col2:
-  st.write("")
+  col1, col2, col3 = st.columns([1,1,1])
 
-with col3:
-  st.write("")
+  with col1:
+    st.image("Website.png", width=900)
 
-st.markdown("<h1 style='text-align: center; color: black;'>WE HELP COMPANIES GROW</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; color: black;'>Become data-driven today and scale your business.We take the time to understand your business and how data can help you to achieve your goals.</h2>", unsafe_allow_html=True)
+  with col2:
+    st.write("")
 
-option = st.sidebar.selectbox("HAB LABS Services", ('Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
+  with col3:
+    st.write("")
+
+  st.markdown("<h1 style='text-align: center; color: black;'>WE HELP COMPANIES GROW</h1>", unsafe_allow_html=True)
+  st.markdown("<h2 style='text-align: center; color: black;'>Become data-driven today and scale your business.We take the time to understand your business and how data can help you to achieve your goals.</h2>", unsafe_allow_html=True)
+
 
 #if option == "Analytics":
   
 #if option == "Data Infrastructure:
   
 if option == "Machine Learning":
-  st.write("Start of ML")
-  
+  st.markdown("<h1 style='text-align: center; color: black;'>PetTech Company</h1>", unsafe_allow_html=True)
+  st.markdown("<h3 style='text-align: left; color: black;'>For this project we were tasked with using the raw data from a 3-axis accelerometer and a 3-axis gyroscope to classify the postion of a dog.</h3>", unsafe_allow_html=True)
+  st.write("")
+  st.markdown("<h3 style='text-align: left; color: black;'>For this use case our algorithim was required to be embedded on a device worn around the dogs neck from which various outputs would alert the dogs owner to any abnormal behaiviour (i.e Lying on side for too long) .</h3>", unsafe_allow_html=True)
 #if option == "Custom Web Apps":
