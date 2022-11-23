9b8e859ce0510bf3743560acbb6138305bfedd44
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-11-23 14:21:55 +0700

    Update project.py

--- project.py
+++ project.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 import requests
 
-#from PIL import Image
-#background = Image.open('Website.png')
-
 option = st.sidebar.selectbox("HAB LABS Services", ( "Home", 'Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
 
 if option == "Home":
@@ -34,17 +31,26 @@ if option == "Machine Learning":
   st.write("")
   st.markdown("<h5 style='text-align: left; color: black;'>For this project we were tasked with using the raw data from a 3-axis accelerometer and a 3-axis gyroscope to classify the postion of a dog.</h5>", unsafe_allow_html=True)
   st.write("")
-  st.markdown("<h5 style='text-align: left; color: black;'>For this use case our algorithim was required to be embedded on a device worn around the dogs neck from which various outputs would alert the dogs owner to any abnormal behaiviour (i.e Lying on side for too long) .</h5>", unsafe_allow_html=True)
+  st.markdown("<h5 style='text-align: left; color: black;'>For this use case our algorithim was required to be embedded on a device worn around the dogs neck from which various outputs would alert the dogs owner to any abnormal behaiviour (i.e Lying on side for too long).</h5>", unsafe_allow_html=True)
   
-if st.button('Run Me'):
- @st.cache(allow_output_mutation=True)
- def load_data():
-  a = pd.read_csv("dog_data (1).csv")
-  return a
- df = load_data()
- st.dataframe(df)
+  st.write("---------"
+  
+  st.markdown("<h2 style='text-align: center; color: black;'>Data Gathering</h1>", unsafe_allow_html=True)
+  st.write("")
+  st.markdown("<h5 style='text-align: left; color: black;'>A crucial part of any Machine Laerning Project is data.</h5>", unsafe_allow_html=True)
+  st.write("")
+  st.markdown("<h5 style='text-align: left; color: black;'>In this example we were able to obtain a publically available dataset (see link to paper). We went through the appropriate data cleaning methodology and arrived at our dataframe which you can see below.</h5>", unsafe_allow_html=True)
+  st.write(""
+  st.markdown("<h5 style='text-align: left; color: black;'>We now had data with which to classify a dog into one of six positions (Sitting, Walking, Standing, Running, Lying down and Eating and drinking).</h5>", unsafe_allow_html=True)
+  
+  if st.button('Run Me and Explore Our Labelled Data Set'):
+    @st.cache(allow_output_mutation=True)
+    def load_data():
+      a = pd.read_csv("dog_data (1).csv")
+    return a
+    df = load_data()
+    st.dataframe(df)
 
-st.markdown("![Foo](https://colab.research.google.com/drive/1sjGgWWfqmBHteGup-PGr0A4XKCkhl8ZR#scrollTo=nt78rXV1zrMn&line=1&uniqifier=1)")
     
   
   
