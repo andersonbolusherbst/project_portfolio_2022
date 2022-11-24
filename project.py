a212b466cdb6562df41d0d2edd24e694284bcc89
Luke Herbst <67044777+LukeHerbst@users.noreply.github.com>
2022-11-24 11:26:53 +0700

    Update project.py

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import altair as alt
 
 option = st.sidebar.selectbox("HAB LABS Services", ( "Home", 'Analytics', 'Data Infrastructure','Machine Learning','Custom Web Apps'))
 
